ff71d3de4d01fea91ed7373bcb7b0bf2ddf0e2bf
enfeeble <ryposte@gmail.com>
2023-07-18 15:00:13 -0400

    page layout

--- script.js
+++ script.js
@@ -1,12 +1,60 @@
-const container = document.querySelector('.grid-container');
-const squares = 16;
+let color = 'black';
+let click = true;
 
-function createGrid() {
-  for (let i = 1; i <= squares; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+function generateBoard(size) {
+  let board = document.querySelector('.board');
+  let squares = board.querySelectorAll('div');
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement('div');
+    square.addEventListener('mouseover', colorSquare);
+    square.style.backgroundColor = 'white';
+    board.insertAdjacentElement('beforeend', square);
   }
 }
 
-createGrid()
+generateBoard(16);
+
+function changeSize(input) {
+  if(input >= 2 && input <= 100) {
+    document.querySelector('.error').style.display = 'none';
+    generateBoard(input)
+  } else {
+    document.querySelector('.error').style.display = 'flex';
+  }
+}
+
+function colorSquare() {
+  if(click) {
+    if(color === 'random') {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
+    }
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function resetBoard() {
+  let board = document.querySelector('.board');
+  let squares = board.querySelectorAll('div');
+  squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+
+document.querySelector('body').addEventListener('click', (e) => {
+  if(e.target.tagName != 'BUTTON') {
+    click = !click;
+    if(click) {
+      document.querySelector('.mode').textContent = "Mode: Etching"
+    } else {
+      document.querySelector('.mode').textContent = "Mode: Not Etching"
+    }
+  }
+});
